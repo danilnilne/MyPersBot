a492ea6a993a6ccdfe6b91d292c8c4eaa49416d3
Danil K <danilnilne@mail.ru>
2020-09-27 13:41:55 +0300

    new st.py file

--- st.py
+++ st.py
@@ -2,22 +2,15 @@ import subprocess
  
 args = ["speedtest-cli"];
 
-/*
-perf['ping',
-*     'dist',
-*     'dspd',
-*     'uspd'];
-*/
 print ("SpeedTest started...");
 
 process = subprocess.Popen(args, stdout=subprocess.PIPE);
 
-output = process.communicate();
+output, error = process.communicate();
 
+print(output);
 
-for line in ta:
-
-    print(element);
+print(error);
 
 print ("Exit!");
 
