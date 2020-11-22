319a73731693ed4f5ea9d2ad5241ecfb07598682
DK <danilnilne@mail.ru>
2020-11-22 21:41:19 +0300

    Update bot_start.py

--- bot_start.py
+++ bot_start.py
@@ -1,42 +1,122 @@
+import time
 import requests;
 import conf;
 
 
-url = 'https://api.telegram.org/bot' + conf.token + '/';
+token = conf.token;
 
-response = requests.get(url + 'getUpdates');
+### GetUpdate function. This fucntion request updates from TAPI. 
+def GetUpdate(token):
+    
+    url = 'https://api.telegram.org/bot' + token + '/' + 'getUpdates';
 
-last_update = 0;
+    response = requests.get(url);
+    
+    return response;
+# GetUpdate END
 
-if (response.status_code == 200):
+### SendMessage function. This fucntion sends data to the chat using TAPI. 
+def SendMessage(token, update):
+    
+        
+    chat_id = str(update['message']['chat']['id']);
+    
+    message = str(update['message']['text']);
+    
+    #https://api.telegram.org/bot[BOT_API_KEY]/sendMessage?chat_id=[MY_CHANNEL_NAME]&text=[MY_MESSAGE_TEXT]
+    url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&text=' + message;
+    
+    print(url);
 
-	data = response.json();
+    #Send Message to the chat
+    try:
+    
+        response = requests.post(url);
+    
+    except:
+    
+        print ("Problems with POST request during Send Message to the chat.");
+    
+    else:
+    
+        #Mark Update as resolved
+    
+        offset = str(update['update_id'] + 1);
+       
+        url = 'https://api.telegram.org/bot' + token + '/' + 'getUpdates?offset=' + offset;
+    
+        try:
+    
+            response = requests.post(url);
+    
+        except:
+    
+            print ("Problems with POST request Mark Update as resolved.");
+    
+        else:
+    
+            return 0;
+    
+# SendMessage END
 
-	try:
+### ParseUpdate function. This fucntion unparse values from the GetUpdate response. 
+def ParseUpdate(response):
 
-		data['result'];
+    if (response.status_code == 200):
+    
+        http_code = 200;
+    
+        print ("HTTP Code is 200"); # Debug logging to the console.
+        
+        data = response.json();
+        
+        try: 
+        
+            data['result'];
+        
+        except: 
+        
+            print ("No updates. Continue GetUpdates");
+        
+        else:
+        
+#            print(data);           # Debug logging to the console.
+            
+            for update in data['result']:
+            
+                ParseCommand(update);
+                
+#                print('=====================')
+#                print ("The Update: ", update['update_id']);
+#                print ("The Message: ", update['message']);
+#                print ("The Message.Chat.ID: ", update['message']['chat']['id']);
+#                print ("The Message.Chat.ID.Text: ", update['message']['text']);
+                           
+    else:
+    
+        print("HTTP request hasn't code 200!");
+        
+        exit(1);  
+# ParseUpdate END
 
-	except Exception:
+### ParseCommand function. This fucntion unparse values from the GetUpdate response. 
+def ParseCommand(update):
 
-		print ("No updates...Exit");
+#     print ('*=====================*')
+#     print ("The Update: ", update['update_id']);
+#     print ("The Message: ", update['message']);
+#     print ("The Message.Chat.ID: ", update['message']['chat']['id']);
+#     print ("The Message.Chat.ID.Text: ", update['message']['text']);
+     SendMessage(token, update)
+# ParseUpdate END
 
-	else:
+### Main 
+while True:
 
-		print(data);
-
-		for update in data['result']:
-
-			print ("--- By Item ---");
-
-			print (update['update_id']);
-
-
-			if ( int(update['update_id']) > last_update):
-
-				last_update = int(update['update_id']);
-
-		print ("Last update is: " + str(last_update));
-
-else:
-
-	print("HTTP request hasn't code 200!");
+    response = GetUpdate(token);
+    
+    ParseUpdate(response);
+    
+    time.sleep(3);    
+    
+# Main END 
