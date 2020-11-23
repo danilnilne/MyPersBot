f6bebfc1da78a92baed70b6057fc84f0170f8e14
Danil K <danilnilne@mail.ru>
2020-11-23 23:55:01 +0300

    Add CheckLogFile function to the dolog module

--- bot_start.py
+++ bot_start.py
@@ -1,105 +1,105 @@
 import time
 import requests;
-import conf;
+import bot_conf;
 
 
-token = conf.token;
+token = bot_conf.token;
 
-### GetUpdate function. This fucntion request updates from TAPI. 
+### GetUpdate function. This fucntion request updates from TAPI.
 def GetUpdate(token):
-    
+
     url = 'https://api.telegram.org/bot' + token + '/' + 'getUpdates';
 
     response = requests.get(url);
-    
+
     return response;
 # GetUpdate END
 
-### SendMessage function. This fucntion sends data to the chat using TAPI. 
+### SendMessage function. This fucntion sends data to the chat using TAPI.
 def SendMessage(token, update):
-    
-        
+
+
     chat_id = str(update['message']['chat']['id']);
-    
+
     message = str(update['message']['text']);
-    
+
     #https://api.telegram.org/bot[BOT_API_KEY]/sendMessage?chat_id=[MY_CHANNEL_NAME]&text=[MY_MESSAGE_TEXT]
     url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&text=' + message;
-    
+
     print(url);
 
     #Send Message to the chat
     try:
-    
+
         response = requests.post(url);
-    
+
     except:
-    
+
         print ("Problems with POST request during Send Message to the chat.");
-    
+
     else:
-    
+
         #Mark Update as resolved
-    
+
         offset = str(update['update_id'] + 1);
-       
+
         url = 'https://api.telegram.org/bot' + token + '/' + 'getUpdates?offset=' + offset;
-    
+
         try:
-    
+
             response = requests.post(url);
-    
+
         except:
-    
+
             print ("Problems with POST request Mark Update as resolved.");
-    
+
         else:
-    
+
             return 0;
-    
+
 # SendMessage END
 
-### ParseUpdate function. This fucntion unparse values from the GetUpdate response. 
+### ParseUpdate function. This fucntion unparse values from the GetUpdate response.
 def ParseUpdate(response):
 
     if (response.status_code == 200):
-    
+
         http_code = 200;
-    
-        print ("HTTP Code is 200. Good connection..."); # Debug logging to the console.
-        
+
+#        print ("HTTP Code is 200. Good connection..."); # Debug logging to the console.
+
         data = response.json();
-        
-        try: 
-        
+
+        try:
+
             data['result'];
-        
-        except: 
-        
+
+        except:
+
             print ("No updates. Continue GetUpdates");
-        
+
         else:
-        
+
 #            print(data);           # Debug logging to the console.
-            
+
             for update in data['result']:
-            
+
                 ParseCommand(update);
-                
+
 #                print('=====================')
 #                print ("The Update: ", update['update_id']);
 #                print ("The Message: ", update['message']);
 #                print ("The Message.Chat.ID: ", update['message']['chat']['id']);
 #                print ("The Message.Chat.ID.Text: ", update['message']['text']);
-                           
+
     else:
-    
+
         print("HTTP request hasn't code 200!");
-        
-        exit(1);  
+
+        exit(1);
 # ParseUpdate END
 
-### ParseCommand function. This fucntion unparse values from the GetUpdate response. 
+### ParseCommand function. This fucntion unparse values from the GetUpdate response.
 def ParseCommand(update):
 
 #     print ('*=====================*')
@@ -110,13 +110,13 @@ def ParseCommand(update):
      SendMessage(token, update)
 # ParseUpdate END
 
-### Main 
+### Main
 while True:
 
     response = GetUpdate(token);
-    
+
     ParseUpdate(response);
-    
-    time.sleep(3);    
-    
-# Main END 
+
+    time.sleep(3);
+
+# Main END
--- dolog.py
+++ dolog.py
@@ -1,10 +1,78 @@
+import bot_conf;
+import os
 
+
+#CheckFile fuction checks provided path if it exist on the disk
+def CheckLogFile(path):
+
+    if os.path.exists(path):
+
+        print('Такой путь существует: ', path);
+
+        return(0);
+
+    else:
+
+        print('Такого пути нет. Создаем...', path);
+
+        path_parts = path.rpartition('/'); # 0: /home/pi/code/tmp/logs; 1: /; 2: bot.log;
+
+        try:
+
+            os.mkdir(path_parts[0]);
+
+        except:
+
+            print("Unable to create dir for log file");
+
+            exit(1);
+
+        else:
+
+            try:
+
+                log_fd = open(path, "a+");
+
+            except:
+
+                print("Unable to create log file.");
+
+                exit(1);
+
+            else:
+
+                print("Log File created.")
+
+
+
+#CheckFile END
 
 #WriteLog fuction provide saving log_strings to the log file
-def WriteLog (module, log_string):
+def WriteLog (log_string):
 
+    log_file = bot_conf.log_file;
+
+    try:
+
+        log_fd = open(log_file, "a+");
+
+    except :
+
+        print("Unable to operate with log file");
+
+        return (1);
+
+    else:
+
+        print(log_file);
+        print(log_string);
     #check if file exits or can be created
-
-    #check if file more than 
-
+    #check if file more than
 #WriteLog END
+
+#Main Body
+
+CheckLogFile(bot_conf.log_file);
+
+WriteLog("Hello!");
+#Main END;
