997a5cdc4c69f8b007303cbfcbd66d1436eeb83d
Danil K <danilnilne@mail.ru>
2020-11-23 22:26:54 +0300

    Add new utility DoLog.py

---
+++ dolog.py
@@ -0,0 +1,10 @@
+
+
+#WriteLog fuction provide saving log_strings to the log file
+def WriteLog (module, log_string):
+
+    #check if file exits or can be created
+
+    #check if file more than 
+
+#WriteLog END
