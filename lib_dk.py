311d4217f355d9c69dac0ef43788c3a9dfd73f0b
Danil K <danilnilne@mail.ru>
2022-04-27 22:25:38 +0300

    Added DB connect function to my lib_dk

--- lib_dk.py
+++ lib_dk.py
@@ -1,5 +1,9 @@
 import subprocess;
 import json;
+import mariadb;
+import sys;
+import bot_conf;
+import time;
 
 ### runShellCommand function runs shell command with options and returns
 # output - command' result
@@ -23,4 +27,43 @@ def runShellCommand (*args):
 
 ### runShellCommand END
 
-print(runShellCommand("speedtest-cli", "--json", "--share"));
+### dbConnect
+# Returns cur - db_cursor pointing to DB
+def dbConnect(**kwargs):
+    # kwargs - keywords arguments, like:
+    # def my_function(**kid):
+    #   print("His last name is " + kid["lname"])
+    #
+    # my_function(fname = "Tobias", lname = "Refsnes")
+    # Connect to MariaDB Platform
+
+    error = "none";
+
+    try:
+        conn = mariadb.connect(
+            user = bot_conf.db_user,
+            password = bot_conf.db_password,
+            host = bot_conf.db_host,
+            port = 3306,
+            database=bot_conf.db_name);
+
+        cur = conn.cursor()
+
+    except mariadb.Error as e:
+
+        error = "Error connecting to MariaDB Platform: " + str(e);
+
+        return error;
+
+    return error, cur;
+
+### dbConnect END
+
+### MAIN
+print(dbConnect());
+
+print(type(dbConnect()));
+
+# print(runShellCommand("speedtest-cli", "--json", "--share"));
+
+### MAIN END
